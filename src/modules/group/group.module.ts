95d6bf2e978eb8dfe1c98016abda74a78128ba91
Nagy László <nagy.laszlo@autsoft.hu>
2020-02-06 14:00:16 +0100

    *

--- src/modules/group/group.module.ts
+++ src/modules/group/group.module.ts
@@ -3,13 +3,14 @@ import {MongooseModule} from "@nestjs/mongoose";
 import {RouteSchema} from "../route/route.schema";
 import {GroupController} from "./group.controller";
 import {GroupService} from "./group.service";
+import {ContextService} from "../../core/services/context.service";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{name: 'Group', schema: RouteSchema}]),
   ],
   controllers: [GroupController],
-  providers: [GroupService],
+  providers: [GroupService, ContextService],
   exports: [GroupService]
 })
 export class GroupModule {
